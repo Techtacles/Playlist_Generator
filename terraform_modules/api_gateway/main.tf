1fb14afbf2193607f601464e0a341b79bd9d8095
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-28 21:03:24 +0100

    Enabling cors for the api gateway

--- terraform_modules/api_gateway/main.tf
+++ terraform_modules/api_gateway/main.tf
@@ -31,7 +31,6 @@ resource "aws_api_gateway_integration" "api_gw_integration" {
 
 resource "aws_api_gateway_deployment" "api_gw_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api_gw_rest.id
-  stage_name  = aws_api_gateway_stage.api_gw_stage.stage_name
 
   triggers = {
     # NOTE: The configuration below will satisfy ordering considerations,

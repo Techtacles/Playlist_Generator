2338b90976e4fba1142482c676c05309284f1ab4
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-28 17:47:40 +0100

    Fixing Endpoint regional location

--- terraform_modules/api_gateway/main.tf
+++ terraform_modules/api_gateway/main.tf
@@ -56,10 +56,3 @@ resource "aws_api_gateway_stage" "api_gw_stage" {
   stage_name    = var.api_gw_stage_name
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.api_gw_rest.id
-  resource_id = aws_api_gateway_resource.api_gw_resource.id
-  http_method = aws_api_gateway_method.api_gw_method.http_method
-  status_code = "200"
-}
-

08bd6e0610a25fb2a48e8c383ad01575e5dc4178
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-28 15:39:41 +0100

    Fixing ecr deployment for lambda

--- terraform_modules/lambda/iam.tf
+++ terraform_modules/lambda/iam.tf
@@ -16,8 +16,4 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = var.lambda_src_file_path
-  output_path = var.lambda_output_zip
-}
+

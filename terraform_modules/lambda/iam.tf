520c0d0fcf0dd17d7721959a630d03dbffdd57a0
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-28 18:18:22 +0100

    Fixing Endpoint regional location

--- terraform_modules/lambda/iam.tf
+++ terraform_modules/lambda/iam.tf
@@ -1,19 +1,44 @@
-data "aws_iam_policy_document" "lambda_assume_role" {
-  statement {
-    effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com","apigateway.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = var.lambda_role
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+  name = var.lambda_role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "playlist_gen_iam_policy"
+  role = aws_iam_role.iam_for_lambda.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "lambda:*",
+          "s3:*",
+          "ecr:*",
+          "events:*",
+          "apigateway:*"
+
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}

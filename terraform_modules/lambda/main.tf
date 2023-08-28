183006b13ef939cc60fa4399a78b5501105b2528
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-28 15:41:12 +0100

    Fixing ecr deployment for lambda

--- terraform_modules/lambda/main.tf
+++ terraform_modules/lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "lambda_fn" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = var.lambda_handler
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  #source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = var.lambda_runtime
 

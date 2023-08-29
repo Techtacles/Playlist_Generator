224ceb458873c807962a10b7d5180a526fed33a3
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-29 06:33:02 +0100

    Enabling cors for the api gateway

--- terraform_modules/api_gateway/main.tf
+++ terraform_modules/api_gateway/main.tf
@@ -110,6 +110,7 @@ resource "aws_api_gateway_integration" "cors_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_gw_rest.id
   type                    = "MOCK"
   integration_http_method = "POST"
+  passthrough_behavior    = "NEVER"
 }
 resource "aws_api_gateway_integration_response" "options" {
   rest_api_id = aws_api_gateway_rest_api.api_gw_rest.id

97692922d4db0f34924d9d45b79b33534ccd6b22
Techtacles <emmanuel.offisong@data2bots.com>
2023-08-29 05:43:39 +0100

    Enabling cors for the api gateway

--- terraform_modules/api_gateway/main.tf
+++ terraform_modules/api_gateway/main.tf
@@ -75,10 +75,10 @@ resource "aws_api_gateway_method_response" "options_200" {
   }
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+
   }
 }
 
@@ -88,4 +88,15 @@ resource "aws_api_gateway_integration" "cors_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_gw_rest.id
   type                    = "MOCK"
   integration_http_method = "POST"
+}
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.api_gw_rest.id
+  resource_id = aws_api_gateway_resource.api_gw_resource.id
+  http_method = aws_api_gateway_integration.cors_integration.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
